c064a08bb642c5f278bf64c4c2f1408ef594f445
CMedina25 <carlosmarsh@hotmail.com>
2023-10-03 10:25:44 -0600

    login

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def test_uno():
     print("Prueba uno")
 
+
 def test_dos():
     print("Prueba uno")
 
+
 def test_tres():
     print("Prueba uno")
+
+
+def test_login():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # Habilita el modo headless
+    chrome_options.add_argument('--disable-gpu')  # Desactiva la GPU para evitar problemas en algunos sistemas
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get('https://www.google.com.mx')
+    driver.quit()
